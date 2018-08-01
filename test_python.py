4b66fa2125dd68d0c66eb8ad62c3d396a295d17f
abhilash <abhilash.s@hcl.com>
2018-07-31 18:58:24 -0800

    get a loop device

--- test_python.py
+++ test_python.py
@@ -6,3 +6,9 @@ print var
 
 var = os.popen('cot install-helpers --verify-only').read()
 print var
+
+loopn = os.popen("/sbin/losetup -f", "r").read().strip()
+if(loopn):
+    print loopn
+else:
+    print "Unable to Get the Free Loop Device" ;
